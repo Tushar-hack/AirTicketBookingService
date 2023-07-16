3f61cae7cb69ab30085df204dc025d4b48033721
Ｔｕｓｈａｒ　Ｔａｋ <tushartak861@gmail.com>
2023-07-16 23:49:12 +0530

    Added create Booking Repository Code

---
+++ src/services/booking-service.js
@@ -0,0 +1,13 @@
+const { BookingRepository } = require('../repository/index');
+
+class BookingService {
+    async create (data) {
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+}
+
+module.exports = BookingService;
